4a8adac6b41873a2f382eb2cd67a3beda4ab314d
d0656146 <d0656146@mail.fcu.edu.tw>
2021-04-19 22:02:04 +0800

    Add some test functions

--- src/objective_function.c
+++ src/objective_function.c
@@ -1,21 +1,72 @@
 #include "objective_function.h"
-#define _USE_MATH_DEFINES
+#define PI (acos(-1))
 
 #include <math.h>
 
+void SetOffset(double new_offset) {
+    offset = new_offset;
+}
+
+double offset = 0.0;
+
 double Ackley_DA(const void* dataset, Vector* vector) {
-    const double a = 20.0, b = 0.2, c = 2.0 * M_PI;
+    const double a = 20.0, b = 0.2, c = 2.0 * PI;
     double sum = 0.0;
     for (int c_dim = 0; c_dim < vector->dimension; c_dim++) {
-        sum += vector->components_ar[c_dim] * vector->components_ar[c_dim];
+        sum += (vector->components_ar[c_dim] - offset) * (vector->components_ar[c_dim] - offset);
     }
-    sum = 0.0 - a * exp(-1.0 * b * sqrt(sum / vector->dimension));
+    sum = 0.0 - a * exp(0.0 - b * sqrt(sum / vector->dimension));
     double cos_sum = 0.0;
     for (int c_dim = 0; c_dim < vector->dimension; c_dim++) {
-        cos_sum += cos(c * vector->components_ar[c_dim]);
+        cos_sum += cos(c * vector->components_ar[c_dim] - offset);
     }
     cos_sum = 0.0 - exp(cos_sum / vector->dimension);
     double result = sum + cos_sum + a + exp(1.0);
     vector->value = 0.0 - result;
+    return vector->value;
+}
+
+double Rastrigin_DA(const void* dataset, Vector* vector) {
+    const double a = 10.0;
+    double sum = 0.0;
+    for (int c_dim = 0; c_dim < vector->dimension; c_dim++) {
+        sum += (vector->components_ar[c_dim] - offset) * (vector->components_ar[c_dim] - offset);
+        sum -= a * cos(2.0 * PI * (vector->components_ar[c_dim] - offset));
+    }
+    sum += a * vector->dimension;
+    vector->value = 0.0 - sum;
+    return vector->value;
+}
+
+double Sphere_DA(const void* dataset, Vector* vector) {
+    double sum = 0.0;
+    for (int c_dim = 0; c_dim < vector->dimension; c_dim++) {
+        sum += (vector->components_ar[c_dim] - offset) * (vector->components_ar[c_dim] - offset);
+    }
+    vector->value = 0.0 - sum;
+    return vector->value;
+}
+
+double Rosenbrock_DA(const void* dataset, Vector* vector) {
+    const double a = 100.0;
+    double sum = 0.0;
+    for (int c_dim = 0; c_dim < vector->dimension - 1; c_dim++) {
+        double x = (vector->components_ar[c_dim + 1] - offset) -
+                   (vector->components_ar[c_dim] - offset) * (vector->components_ar[c_dim] - offset);
+        sum += a * x * x;
+        sum += (vector->components_ar[c_dim] - offset - 1) * (vector->components_ar[c_dim] - offset - 1);
+    }
+    vector->value = 0.0 - sum;
+    return vector->value;
+}
+double Michaelwicz_DA(const void* dataset, Vector* vector) {
+    const double m = 10.0;
+    double sum = 0.0;
+    for (int c_dim = 0; c_dim < vector->dimension; c_dim++) {
+        double sqr = (vector->components_ar[c_dim] - offset) * (vector->components_ar[c_dim] - offset);
+        double sin_sum = sin(sqr * (c_dim + 1) / PI);
+        sum -= sqr * pow(sin_sum, 2.0 * m);
+    }
+    vector->value = 0.0 - sum;
     return vector->value;
 }
--- src/objective_function.h
+++ src/objective_function.h
@@ -1,11 +1,19 @@
 /**
  * Some objective functions of continuous optimization problem
  */
-#ifndef OBJECT_FUNCTION_H_
-#define OBJECT_FUNCTION_H_
+#ifndef OBJECTIVE_FUNCTION_H_
+#define OBJECTIVE_FUNCTION_H_
 
 #include "problem_solution.h"
 
-double Ackley_DA(const void* dataset, Vector* vector);
+extern double offset;
 
-#endif  // OBJECT_FUNCTION_H_
+void SetOffset(double new_offset);
+
+double Ackley_DA(const void* dataset, Vector* vector);
+double Rastrigin_DA(const void* dataset, Vector* vector);
+double Sphere_DA(const void* dataset, Vector* vector);
+double Rosenbrock_DA(const void* dataset, Vector* vector);
+double Michaelwicz_DA(const void* dataset, Vector* vector);
+
+#endif  // OBJECTIVE_FUNCTION_H_
